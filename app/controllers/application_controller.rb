b11f365f9f1ddc3b936f47ea0ee2cc5877c35f58
Paul Straw <paulstraw@paulstraw.com>
2016-03-01 00:09:46 -0800

    Set Strict-Transport-Secure header for HTTPS.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_hsts_header
 
+private
+  def set_hsts_header
+    if Rails.env.production?
+      response.headers['Strict-Transport-Security'] = "max-age=31536000"
+    end
+  end
 end
