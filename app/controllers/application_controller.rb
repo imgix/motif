d3d3a9bad66ab0e965e02474241830c260d9a634
Paul Straw <paulstraw@paulstraw.com>
2016-03-01 00:21:56 -0800

    Remove manual HSTS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :set_hsts_header, if: -> { Rails.env.production? }
 
-private
-  def set_hsts_header
-    response.headers['Strict-Transport-Security'] = "max-age=31536000"
-  end
 end
