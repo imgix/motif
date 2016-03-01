375ffbbf253791cff682e44fe6228a7de7eb0b1d
Paul Straw <paulstraw@paulstraw.com>
2016-03-01 00:15:44 -0800

    force_ssl syntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
-  force_ssl if: Rails.env.production?
-  before_action :set_hsts_header, if: Rails.env.production?
+  force_ssl if: :prod?
+  before_action :set_hsts_header, if: :prod?
 
 private
+  def prod?
+    Rails.env.production?
+  end
+
   def set_hsts_header
     response.headers['Strict-Transport-Security'] = "max-age=31536000"
   end
